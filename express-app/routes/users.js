539716ec20f98eac7d1b62a1e6984ca44688d3bc
br41nd34d <danil.pashkov.45@gmail.com>
2026-03-24 17:58:43 +0500

    8

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,40 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
-const users = [
-    {
-        id: 1,
-        name: "Юра"
-    },
-    {
-        id: 2,
-        name: "Даня"
-    }]
-
-let userId = 3;
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('mydb.db');
+db.run(`CREATE TABLE IF NOT EXISTS users (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name text)`);
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.send(users);
+    db.all("SELECT id, name FROM users", [], (err, rows) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(rows);
+        }
+    });
 });
 
 router.post('/', function (req, res, next) {
-    const newUser = {
-        id: userId,
-        name: req.body.name,
-    }
-    userId++;
-    users.push(newUser);
-    res.status(201).json(newUser);
+    const name = req.body.name;
+    const insert = "INSERT INTO users (name) VALUES (?)";
+    db.run(insert, [name]);
+    res.status(201).json(name);
 });
 
 router.get('/:id', function (req, res, next) {
-    let user = users.find(user => user.id === Number(req.params.id));
-    if (!user) {
-        res.status(404).json({})
-        return;
-    }
-    res.send(user);
+    const id = Number(req.params.id);
+    db.all("SELECT id, name FROM users WHERE id = ?", [id], (err, rows) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(rows);
+        }
+    });
 })
 
 module.exports = router;
