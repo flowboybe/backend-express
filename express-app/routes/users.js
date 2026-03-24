89c19d634e6c7fb24be7bd07fde5edd17bd3b92e
YurLea <vsailevyrui@gmail.com>
2026-03-24 15:17:46 +0300

    5-th done

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+const users = [{
+    id: 1,
+    name: "Юра"
+},
+    {
+        id: 2,
+        name: "Даня"
+    }]
+
+let userId = 3;
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.send([{
-        id: 1,
-        name: "Юра"
-    },
-        {
-            id: 2,
-            name: "Даня"
-        }])
+    res.send(users);
+});
+
+router.post('/', function (req, res, next) {
+    const newUser = {
+        id: userId,
+        name: req.body.name,
+    }
+    userId++;
+    users.push(newUser);
+    res.status(201).json(newUser);
 });
 
 module.exports = router;
